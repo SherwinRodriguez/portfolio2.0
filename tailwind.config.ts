1c700214ce3663944f99da8de2c45bf65168b28a
SherwinRodriguez <sherwin7rodriguez10@gmail.com>
2025-12-27 18:09:43 +0530

    Initial setup: Add project configuration and dependencies

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+        blob: "blob 8s infinite ease-in-out",
+      },
+      keyframes: {
+        blob: {
+          "0%, 100%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+        },
+      },
+    },
+  },
+  plugins: [
+     require('@tailwindcss/line-clamp')
+  ],
+};
